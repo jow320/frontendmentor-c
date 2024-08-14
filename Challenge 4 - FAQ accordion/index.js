bbe732af2ebcfc6c9259bc43ccb388aaa312edf3
Victor Estevan <jow_d3@hotmail.com>
2024-08-14 12:31:42 -0300

    fem140824

--- Challenge 4 - FAQ accordion/index.js
+++ Challenge 4 - FAQ accordion/index.js
@@ -2,15 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
   const toggles = document.querySelectorAll("[id^='toggle']");
   const hiddenParagraphs = document.querySelectorAll("[id^='hidden']");
 
-  toggles.forEach((toggle, index) => {
-    toggle.addEventListener("click", () => {
-      const hiddenParagraph = document.getElementById(`hidden${index + 1}`);
+  toggles.forEach((toogle, i) => {
+    toogle.addEventListener("click", () => {
+      const hiddenParagraph = document.getElementById(`hidden${i + 1}`);
       if (hiddenParagraph.classList.contains("hidden")) {
         hiddenParagraph.classList.remove("hidden");
-        toggle.src = "./faq-accordion-main/assets/images/icon-minus.svg";
+        toogle.src = "./faq-accordion-main/assets/images/icon-minus.svg";
       } else {
         hiddenParagraph.classList.add("hidden");
-        toggle.src = "./faq-accordion-main/assets/images/icon-plus.svg";
+        toogle.src = "./faq-accordion-main/assets/images/icon-plus.svg";
       }
     });
   });
